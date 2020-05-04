862360b4f469119b1d8e9974e485f066afa1169a
HMVarshney <30654000+HMVarshney@users.noreply.github.com>
2020-05-05 03:42:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,4 @@ import ReactDOM from 'react-dom';
 
 import Main from './main'
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-// PATH CONFIGURATION TO RESPOND TO A REQUEST TO STATIC ROUTE REQUEST BY SERVING index.html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(80);
-console.log('Server is listening on http://localhost:5000');
-
 ReactDOM.render(<Main />, document.getElementById('root'));
