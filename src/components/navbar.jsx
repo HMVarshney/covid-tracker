836e1317b5fd561b61111056d46a351a11b3c98c
HMVarshney <himank.varshney9@gmail.com>
2020-05-07 14:21:03 +0530

    Debug#2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import StyleSheet from 'react'
 
 class NavBar extends Component {
     state = {  }
     render() { 
         return ( 
-            <nav class="navbar navbar-expand-lg bg-dark">
-            <h4><a class="navbar-brand badge badge-primary" href="/">Covid-Tracker</a></h4>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active text-white" href="/india">For India</a>
-                    <a class="nav-link active text-white" href="/about">About</a>
+            <nav className="navbar navbar-expand-lg bg-dark">
+            <h4><a className="navbar-brand badge badge-primary" href="/">Covid-Tracker</a></h4>
+                <div className="navbar-nav">
+                    <h6><a className="nav-link active text-white" href="/india">For India</a></h6>
+                    <h6><a className="nav-link active text-white" href="/about">About</a></h6>
                 </div>
-            </div>
             </nav>
          );
     }
